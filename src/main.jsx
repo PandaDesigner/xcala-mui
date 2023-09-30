a199fb57259c6e3d95c41232c4b5fa91491dd2f0
Pedro Fernandez <pedro@pandadesigners.com>
2023-09-30 12:35:47 -0500

    portafolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { ThemeProvider, useTheme } from "@mui/material";
-import { theme } from "./theme.js";
-import { NewDashboard } from './components/newDasborad/NewDashboard.jsx'
-import { ProductView } from "./components/newViewPortafolio/ProductView.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import {ThemeProvider, useTheme} from '@mui/material'
+import {theme} from './theme.js'
+import {NewDashboard} from './components/newDasborad/NewDashboard.jsx'
+import {ProductView} from './components/newViewPortafolio/ProductView.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			{/* <NewDashboard /> */}
-			<ProductView />
-		</ThemeProvider>
-	</React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* <NewDashboard /> */}
+      <ProductView />
+    </ThemeProvider>
+  </React.StrictMode>
+)
